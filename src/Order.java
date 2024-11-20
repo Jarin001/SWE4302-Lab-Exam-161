adc6b43e1939d8722e14d78e8bbc323295e85213
Jarin001 <snehasenmoni.5october@gmail.com>
2024-11-20 16:22:07 +0600

    Task2 iv

--- src/Order.java
+++ src/Order.java
@@ -1,3 +1,25 @@
 public class Order {
+    String flavour;
+    String toppings;
+    String cone;
+    double subtotal=0.00;
+
+    public Order (String flavour, String toppings, String cone, double subtotal){
+        this.flavour=flavour;
+        this.toppings=toppings;
+        this.cone=cone;
+        this.subtotal=subtotal;
+    }
+
+    public double coneprice(String type){
+        if(type=="Waffle"){
+            subtotal+=5.00;
+        }
+        else subtotal +=0.00;
+
+        return subtotal;
+    }
+
+    
 
 }
